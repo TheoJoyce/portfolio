51de75c918d1c72e66d68309ddd3c61dcd9e7c4f
Theo Joyce <theo@theojoyce.com>
2020-02-20 15:34:39 -0500

    Implemented PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './index.html',
+        './components/*.js',
+        './pages/*.js'
+      ],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
     plugins: [
         require('tailwindcss'), 
         require('postcss-preset-env'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
     ]
 }
