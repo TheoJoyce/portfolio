6252fe08b95ba9e7fb0b8db82da27f703b86c95b
TheoJoyce <theo@theojoyce.com>
2021-01-23 19:42:49 -0500

    Implemented a toggle for dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FormspreeProvider } from '@formspree/react'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
 import '../styles/index.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <FormspreeProvider project="1519626967953964241">
-            <Component {...pageProps} />
+            <ThemeProvider attribute="class">
+                <Component {...pageProps} />
+            </ThemeProvider>
         </FormspreeProvider>
     )
 }
